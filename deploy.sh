cb8a5b602a18aff3255b0d9e953e31f7b3636196
Junya Yamada <mtjuney@gmail.com>
2018-04-17 23:58:59 +0900

    セットアップとデプロイ

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+# どどんとふをコピー
+cp -r DodontoF /var/www/html/
+rsync -av DodontoF/ /var/www/html/DodontoF/
+
+# 権限変更
+chmod 705 -R /var/www/html/DodontoF
+chmod 703 -R /var/www/html/DodontoF/saveDataTempSpace
+chmod 703 -R /var/www/html/DodontoF/fileUploadSpace
+chmod 703 -R /var/www/html/DodontoF/replayDataUploadSpace
+chmod 606 -R /var/www/html/DodontoF/log.*
+# chmod 707 -R /var/www/html/imageUploadSpace
+# chmod 707 -R /var/www/saveData
--- setup.sh
+++ setup.sh
@@ -1,21 +1,12 @@
 yum update -y && yum -y upgrade
 
-# # どどんとふをコピー
-cp -r www /var/
-
 yum install -y httpd
 yum install -y ruby rubygems
 ln -s /usr/bin/ruby /usr/local/bin/ruby
 gem update --system
 
-# 権限変更
-chmod 705 -R /var/www/html/DodontoF
-chmod 703 -R /var/www/html/DodontoF/saveDataTempSpace
-chmod 703 -R /var/www/html/DodontoF/fileUploadSpace
-chmod 703 -R /var/www/html/DodontoF/replayDataUploadSpace
-chmod 606 -R /var/www/html/DodontoF/log.*
-chmod 707 -R /var/www/html/imageUploadSpace
-chmod 707 -R /var/www/saveData
+cp -r imageUploadSpace /var/www/html/
+cp -r saveData /var/www/
 
 # httpdの設定
 echo \<Directory "/var/www/html/DodontoF"\> >> /etc/httpd/conf/httpd.conf
@@ -28,5 +19,4 @@ echo \</Directory\> >> /etc/httpd/conf/httpd.conf
 firewall-cmd --permanent --zone public --add-service http
 systemctl enable httpd
 
-# /etc/init.d/httpd start
 systemctl start httpd
